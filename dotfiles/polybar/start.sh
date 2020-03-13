c50ef85a954171964054db6a54a5c367350e6796
CramMK <ma_tho@web.de>
2020-03-13 11:06:39 +0100

    Clean polybar

--- dotfiles/polybar/start.sh
+++ dotfiles/polybar/start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-killall polybar
-polybar mybar
+# Kill old polybars
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+laptop=$(xrandr --query | grep 'eDP-1')
+
+if [[ ${laptop} = *connected* ]]; then
+    polybar laptop &
+fi
