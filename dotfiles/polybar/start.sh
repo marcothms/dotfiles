7ec492dbbc82d577e97ff9b74719a1d84ff5a01e
CramMK <ma_tho@web.de>
2020-03-20 09:58:31 +0100

    Fix startscript for polybar

--- dotfiles/polybar/start.sh
+++ dotfiles/polybar/start.sh
@@ -6,7 +6,12 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch
 laptop=$(xrandr --query | grep 'eDP-1')
+hdmi1=$(xrandr --query | grep 'HDMI-1')
 
 if [[ ${laptop} = *connected* ]]; then
     polybar laptop &
+fi
+
+if [[ ${hdmi1} = *connected* ]]; then
+    polybar HDMI1 &
 fi
