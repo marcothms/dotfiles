353b4e2ad3df5b8440d77798656da6489b26d5f4
CramMK <ma_tho@web.de>
2020-04-21 16:21:26 +0200

    Update polybar launch

--- dotfiles/polybar/start.sh
+++ dotfiles/polybar/start.sh
@@ -1,19 +1,16 @@
-#!/bin/bash
-
-# Kill old polybars
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch
-laptop=$(xrandr --query | grep 'eDP-1')
-hdmi1=$(xrandr --query | grep 'HDMI-1')
-
-if [[ ${laptop} = *connected* ]]; then
-    polybar laptop_top &
-    polybar laptop_bottom &
-fi
-
-if [[ ${hdmi1} = *connected* ]]; then
-    polybar HDMI1_top &
-    polybar HDMI1_bottom &
+if type "xrandr"; then
+  for mon in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [[ ${mon} = *eDP-1* ]]; then
+        polybar --reload laptop_top &
+        polybar --reload laptop_bottom &
+    else
+        MONITOR=$mon polybar --reload external_top &
+        MONITOR=$mon polybar --reload external_bottom &
+    fi
+  done
+else
+    echo "No Bars loaded."
 fi
