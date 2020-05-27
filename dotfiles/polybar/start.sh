67714a08824e5d91df083db206bd62a94875c8e8
CramMK <ma_tho@web.de>
2020-05-27 14:09:40 +0200

    Some changes

--- dotfiles/polybar/start.sh
+++ dotfiles/polybar/start.sh
@@ -4,11 +4,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for mon in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     if [[ ${mon} = *eDP-1* ]]; then
-        polybar --reload laptop_top &
-        #polybar --reload laptop_bottom &
+        polybar --reload laptop &
     else
-        MONITOR=$mon polybar --reload external_top &
-        #MONITOR=$mon polybar --reload external_bottom &
+        MONITOR=$mon polybar --reload external &
     fi
   done
 else
