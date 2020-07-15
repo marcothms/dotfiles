fd69f28b34c1d92da3ef57da597df9a92dfc5d77
CramMK <ma_tho@web.de>
2020-07-15 20:18:46 +0200

    Update polybar

--- dotfiles/polybar/start.sh
+++ dotfiles/polybar/start.sh
@@ -3,11 +3,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for mon in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [[ ${mon} = *eDP-1* ]]; then
-        polybar --reload main &
-    else
-        MONITOR=$mon polybar --reload main &
-    fi
+    MONITOR=$mon polybar --reload main &
   done
 else
     echo "No Bars loaded."
